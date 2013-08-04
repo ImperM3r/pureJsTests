8c1ad212fdcd631dbf4d66b3c7f7766673ac7d9a
«Sergey» <imperm3r.main@gmail.com>
2013-08-04 17:44:53 +0400

    Исправление форматирования кода

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var clockInConsole = {
-    showClock : function(intervalName) {
+    showClock: function(intervalName) {
         var date = new Date(),
             hours = date.getHours(),
             minutes = date.getMinutes(),
@@ -10,17 +10,17 @@ var clockInConsole = {
         if (seconds == 59)
             clearInterval(intervalName);
     },
-    
-    clearConsole : function() {
+
+    clearConsole: function() {
         console.clear();
     },
-    
-    declOfNum : function(number, titles) {  
-    cases = [2, 0, 1, 1, 1, 2];  
-    return number + " " + titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
-    }  
+
+    declOfNum: function(number, titles) {
+        cases = [2, 0, 1, 1, 1, 2];
+        return number + " " + titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+    }
 }
 
-var showClock = setInterval (function() {
+var showClock = setInterval(function() {
     clockInConsole.showClock(showClock)
-    }, 1000);
+}, 1000);
