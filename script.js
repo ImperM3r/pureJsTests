bd4442ddac576e410733c5d204a47bfffab600f4
«Sergey» <imperm3r.main@gmail.com>
2013-08-04 18:09:47 +0400

    Добавлено замыкание, обалсть видимости ограничена в переменной clockInConsole

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-var clockInConsole = {
-    showClock: function(intervalName) {
-        var date = new Date(),
-            hours = date.getHours(),
-            minutes = date.getMinutes(),
-            seconds = date.getSeconds(),
-            currentTime = "Время: " + this.declOfNum(hours, ['часа', 'часов', 'часов']) + " " + this.declOfNum(minutes, ['минута', 'минуты', 'минут']) + " " + this.declOfNum(seconds, ['секунда', 'секунды', 'секунд']);
-        this.clearConsole();
-        console.log(currentTime);
-        if (seconds == 59)
-            clearInterval(intervalName);
-    },
-
-    clearConsole: function() {
-        console.clear();
-    },
-
-    declOfNum: function(number, titles) {
-        cases = [2, 0, 1, 1, 1, 2];
-        return number + " " + titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+﻿var clockInConsole = (function() {
+    
+    return {
+        showClock: function(intervalName) {
+            var date = new Date(),
+                hours = date.getHours(),
+                minutes = date.getMinutes(),
+                seconds = date.getSeconds(),
+                currentTime = "Время: " + declOfNum(hours, ['часа', 'часов', 'часов']) + " " + declOfNum(minutes, ['минута', 'минуты', 'минут']) + " " + declOfNum(seconds, ['секунда', 'секунды', 'секунд']);
+            clearConsole();
+            console.log(currentTime);
+            if (seconds == 59)
+                clearInterval(intervalName);
+        }
     }
-}
+
+        function clearConsole() {
+            console.clear();
+        }
+
+        function declOfNum(number, titles) {
+            cases = [2, 0, 1, 1, 1, 2];
+            return number + " " + titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+        }
+
+})();
 
 var showClock = setInterval(function() {
     clockInConsole.showClock(showClock)
